526b1869c5fb3ddfe6b6e70f8645e14102812c6d
sio <127683092+sioXD@users.noreply.github.com>
2025-05-26 19:47:02 +0200

    Update pageEE.js

--- pageEE.js
+++ pageEE.js
@@ -4,15 +4,16 @@ export function renderEE() {
     <head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Easter Egg</title>
-    </head>
     <style>
       body {
         text-align: center;
         background-color: #f0f0f0;
         font-family: Arial, sans-serif;
       }
-        iframe {
-      margin: 20px;
+      iframe {
+        margin: 20px;
+        width: 80%;
+        height: 50%;
       }
 
       .button {
@@ -34,10 +35,9 @@ export function renderEE() {
       max-width: 300px;
     }
   }
-
-
     </style>
-      <body>
+    </head>
+    <body>
         <h1>Das ist ein Easter Egg!</h1>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="" frameBorder="0"   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"  allowFullScreen></iframe>  <br>      
       
