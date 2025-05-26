027e99c322c6312943732b6fa6358652d7ce5fe2
sio <127683092+sioXD@users.noreply.github.com>
2025-05-26 19:54:28 +0200

    Update pageLogin.js

--- pageLogin.js
+++ pageLogin.js
@@ -3,6 +3,7 @@ export function renderLogin(login) {
     <html>
       <head>
         <title>Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
           body {
             text-align: center;
